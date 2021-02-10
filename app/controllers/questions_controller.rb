af302f45c76b04a5b9a12ebc1ff024233c299bd7
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-02-09 21:33:36 -0500

    Progress on the answers page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @query = params[:question]
+    @answer = ''
+    if @query.downcase == 'i am going to work right now!'
+      @answer = 'Great!'
+    elsif @query.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
